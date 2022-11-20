0e21a1c191a462c9f0f10237da19de7261c3f4c1
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-11-20 22:20:58 +0900

    index.js 수정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {Provider} from "react-redux";
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import { Router } from 'react-router-dom';
+import Reducer from './redux/reducers';
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+ 
+     
     <App />
+    
+    
   </React.StrictMode>
 );
 
@@ -15,3 +25,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
