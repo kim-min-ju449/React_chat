273f017edfb77d1c726e8bce62029859524a5642
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-11-21 20:20:56 +0900

    회원가입 UI 구현

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
 import { Router } from 'react-router-dom';
 import Reducer from './redux/reducers';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
- 
+    
+    <Provider store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
      
     <App />
-    
+    </Provider>
     
   </React.StrictMode>
 );
