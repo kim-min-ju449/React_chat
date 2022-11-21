7ce8646edf0c4f0d22ea4bfb499a0428f8465f7c
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-11-21 20:20:02 +0900

    회원가입 UI 구현

--- src/components/ReguisterPage/RegisterPage.js
+++ src/components/ReguisterPage/RegisterPage.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
 function RegisterPage(){
     return(
-        <div>
-            RegisterPage
+        <div className="auth-wrapper">
+            <div style={{textAlign:'center'}}>
+                <h3>Register</h3>
+            </div>
+             <form >
+                <label>Email</label>
+      <input name="email" type="email"/>
+      <label>Name</label>
+      <input name="name" type="name"/>
+      <label>Password</label>
+      <input name="password" type="password"/>
+      <label>Password Confirm</label>
+      <input name="password_confim" type="password"/>
+
+      {/* errors will return when field validation fails  */}
+      {/* {errors.exampleRequired && <p>This field is required</p>} */}
+
+      <input type="submit" />
+    </form>
+    <Link style={{color: 'gray' , textDecoration:'none'}} to="/login">이미 아이디가 있다면</Link>
         </div>
     )
 }
