a5148209f39258dce782f74b008ce9d34a25db66
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-11-24 20:06:37 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, {useEffect} from "react";
+import firebase from './firebase';
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route, Link, useNavigate} from "react-router-dom"; 
+import ChatPage from './components/ChatPage/ChatPage';
+import LoginPage from './components/Loginpage/Loginpage';
+import RegisterPage from './components/ReguisterPage/RegisterPage';
+import { useDispatch, useSelector } from "react-redux";
+import { setUser, clearUser } from "./redux/actions/user_action";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 function App() {
+
+  //const navigate = useNavigate();
+  //let dispatch = useDispatch();
+  //const isLoading = useSelector((state) => state.user.isLoading);
+
+  // useEffect(() => {
+  //   const auth = getAuth();
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       navigate("/");
+  //       dispatch(setUser(user));
+  //       const uid = user.uid;
+  //       // ...
+  //     } else {
+  //       navigate("/login");
+  //       dispatch(clearUser());
+  //       // User is signed out
+  //       // ...
+  //     }
+  //   });
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<ChatPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+</Routes>
+    </Router>
   );
 }
 
