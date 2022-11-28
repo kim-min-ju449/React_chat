700aae00ced23470662b0d8e7f441bf1965d3b04
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-11-28 19:46:30 +0900

    프로필 사진 수정

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useRef} from "react";
 import {IoIosChatboxes} from 'react-icons/io';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Image from 'react-bootstrap/Image';
@@ -8,8 +8,17 @@ import firebase from "../../../firebase";
 function UserPanel(){
     const user = useSelector(state => state.user.currentUser);
 
+    const inputOpenImageRef = useRef();
+
     const handleLogout = () =>{
         firebase.auth().signOut();
+    }
+
+    const handleOpenImageRef = () =>{
+        inputOpenImageRef.current.click();
+    }
+    const handleUploadImage = () =>{
+        
     }
     return(
         <div>
@@ -19,6 +28,8 @@ function UserPanel(){
                 <Image src={user && user.photoURL} 
                 style={{width:'30px', height:'30px', marginTop:'3px'}}
                 roundedCircle/>
+
+                <input style={{display:"none"}} type="file" ref={inputOpenImageRef} accept="image/jpeg, image/png" onChange={handleUploadImage}/>
                 <Dropdown>
                     <Dropdown.Toggle 
                     style={{background: 'transparent', border:'0px'}}
@@ -26,7 +37,7 @@ function UserPanel(){
                         {user && user.displayName}
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">프로필 사진 변경</Dropdown.Item>
+                        <Dropdown.Item onClick={handleOpenImageRef}>프로필 사진 변경</Dropdown.Item>
                         <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
                         
                     </Dropdown.Menu>
